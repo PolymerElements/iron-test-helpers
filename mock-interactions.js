8b4f9717f07439e22b438b9f913d194c582fd9a4
Limon Monte <limonte@users.noreply.github.com>
2017-01-05 09:54:24 +0200

    Add move() to the MockInteractions scope

--- mock-interactions.js
+++ mock-interactions.js
@@ -448,6 +448,7 @@
   scope.up = up;
   scope.downAndUp = downAndUp;
   scope.tap = tap;
+  scope.move = move;
   scope.touchstart = touchstart;
   scope.touchend = touchend;
   scope.makeSoloTouchEvent = makeSoloTouchEvent;
